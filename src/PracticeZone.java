73a9e8663a57e902124feac543251a6a5a030a84
JohnIraCole <budoycole123@gmail.com>
2026-03-18 20:33:05 +0800

    Practiced with 2D arrays

--- src/PracticeZone.java
+++ src/PracticeZone.java
@@ -2,27 +2,37 @@ import java.util.Scanner; // For input/output operations
 
 public class PracticeZone{
 	public static void main(String[] args){
-				Scanner sc = new Scanner(System.in);
+		java.util.Scanner sc = new Scanner(System.in);
 
-				double[] scoreArr = new double[5];
+		
+		int sum1 = 0;
+		int sum2 = 0;
+		int sum3 = 0;
 
-				for(int i = 0; i < 5; i++){
-					System.out.print("Enter score #" + (i + 1)+ ": ");
-					double score = sc.nextDouble();
-					scoreArr[i] = score;
+		int[][] arr = {{1,2,3},
+					   {4,5,6},
+					   {7,8,9}}; 
+		
+		for(int i = 0; i < arr.length;i++){
+			for(int j = 0; j < arr[i].length; j++){
+				if(arr[i] == arr[0]){
+					sum1 += arr[i][j];
 				}
-
-				System.out.println("Avarege score: " + average(scoreArr));
-
-				sc.close();
-	}
-	static double average(double... number){
-		double res = 0;
-
-		for(double numbers: number){
-			res += numbers;
+				else if(arr[i] == arr[1]){
+					sum2 += arr[i][j];
+				}
+				else{
+					sum3 += arr[i][j];
+				}
+			}
+			
 		}
+		System.out.println("Sum of first array: " + sum1);
+		System.out.println("Sum of second array: " + sum2);
+		System.out.println("Sum of third array: " + sum3);
 
-		return res / number.length;
+		sc.close();
 	}
+
+	
 }
---
+++ src/TwoDimensional.java
@@ -0,0 +1,79 @@
+import java.util.Scanner; // For input/output operations
+
+public class TwoDimensional{
+	public static void main(String[] args){
+		java.util.Scanner sc = new Scanner(System.in);
+
+		// **** SUM OF ELEMENTS ****
+		/* 
+		int sum = 0;
+		int[][] arr = {{1,2,3},
+					   {4,5,6},
+					   {7,8,9}}; 
+
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[i].length; j++){
+				sum += arr[i][j];
+			}
+		}
+
+		System.out.println("Sum: " + sum );
+		*/
+		
+		// **** SUM OF ROWS OF EACH ROWS ****
+		/* 
+		int sum1 = 0;
+		int sum2 = 0;
+		int sum3 = 0;
+
+		int[][] arr = {{1,2,3},
+					   {4,5,6},
+					   {7,8,9}}; 
+		
+		for(int i = 0; i < arr.length;i++){
+			for(int j = 0; j < arr[i].length; j++){
+				if(arr[i] == arr[0]){
+					sum1 += arr[i][j];
+				}
+				else if(arr[i] == arr[1]){
+					sum2 += arr[i][j];
+				}
+				else{
+					sum3 += arr[i][j];
+				}
+			}
+			
+		}
+		System.out.println("Sum of first array: " + sum1);
+		System.out.println("Sum of second array: " + sum2);
+		System.out.println("Sum of third array: " + sum3);
+		*/
+		
+		// **** FIND MIN AND MAX ****
+		/*int[][] arr = {{9,2,3},
+					   {4,5,6},
+					   {7,8,1}}; 
+
+		int min = arr[0][0];
+		int max = arr[0][0];
+
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[i].length; j++){
+				if(arr[i][j] > max){
+					max = arr[i][j];
+				}
+				if(arr[i][j] < min){
+					min = arr[i][j];
+				}	
+			}
+		}
+
+		System.out.println(max);
+		System.out.println(min);
+		*/
+		
+		sc.close();
+	}
+
+	
+}

4cf3a65a537f28a8c40bfa2c7786dfd45da0e5b0
JohnIraCole <budoycole123@gmail.com>
2026-03-27 18:34:33 +0800

    Practiced Inheritance and practiced array methods(in TextPad)

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -3,32 +3,86 @@ import java.util.Scanner;
 public class Inheritance{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        
-        dog.display();
-        cat.display();
+        Dog dog = new Dog("Shitzu");
+        Cat cat = new Cat("Persian");
+
+        Animal[] types = {dog,cat};
+
+        for(Animal elements : types){
+            System.out.println(elements.speciesName);
+            elements.sound();    
+        }
 
         sc.close();
     }
 }
 // GRANDPARENT
+class Organisms{
+    String speciesName;
 
- // PARENT
-class Animal{
-
-    boolean isAlive = true;
-
-    void display(){
-        System.out.println("This animal is alive: " + isAlive);
+    Organisms(String name){
+        this.speciesName = name;
     }
+
+    void showSpecies(){
+        System.out.println("I am a " + speciesName);
+    }
+}
+
+ // PARENTS
+class Plants extends Organisms{
+
+    Plants(String name){
+        super(name);
+    }
+
+    void photosynthesized(){
+        System.out.println("The plant is getting energy from the sun.");
+    }
+
+}
+
+abstract class Animal extends Organisms{
+
+    Animal(String name){
+        super(name);
+    }
+
+    void move(){
+        System.out.println("The animal walks");
+    }
+
+    abstract void sound();
+    
 }
 
 // CHILD
 class Dog extends Animal{
+    int lives = 1;
 
+    Dog(String name){
+        super(name);
+    }
+
+    void sound(){
+        System.out.println("The dog barks");
+    }
 }
 
 class Cat extends Animal{
+    int lives = 9;
 
+    Cat(String name){
+        super(name);
+    }
+
+    void sound(){
+        System.out.println("The cat meowed");
+    }
+
+    @Override
+    void move(){
+        super.move();
+        System.out.println("The cat leaps gracefully");
+    }
 }
